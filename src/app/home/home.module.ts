ba6a9af740722be0d24d61af783ff0d6c0782362
unknown <zakaria.khalil1999@gmail.com>
2020-11-22 20:16:46 +0100

    dernière MAJ

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { DragulaModule } from 'ng2-dragula';
 import { HomePageRoutingModule } from './home-routing.module';
 
 
 @NgModule({
   imports: [
+    
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    DragulaModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,72 @@
 import { Component } from '@angular/core';
-
+import { DragulaService } from 'ng2-dragula';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  q1 = [
+    { value: "Réviser pour l'examen", color: 'primary' },
+    
+  ];
+  q2 = [
+    
+    { value: 'S\'entrainer ', color: 'secondary' }
+  ];
+  q3 = [
+    { value: 'Répondre aux messages sur WhatsApp', color: 'tertiary' },
+    
+  ];
+  q4 = [
+    { value: 'Jouer en ligne', color: 'warning' },
+    { value: 'Regarder Game of Thrones', color: 'warning' }
+  ];
+ 
+  todo = { value: '', color: '' };
+  selectedQuadrant = 'q1';
+  constructor(private dragulaService: DragulaService,private toastController: ToastController) {
+    this.dragulaService.drag('bag')
+    .subscribe(({ name, el, source }) => {
+      el.setAttribute('color', 'danger');
+    });
+ 
+    this.dragulaService.removeModel('bag')
+    .subscribe(({ item }) => {
+      this.toastController.create({
+        message: 'Supprimé: ' + item.value,
+        duration: 2000
+      }).then(toast => toast.present());
+    });
+ 
+    this.dragulaService.dropModel('bag')
+      .subscribe(({ item }) => {
+        item['color'] = 'success';
+      });
+ 
+    this.dragulaService.createGroup('bag', {
+      removeOnSpill: true
+    });
+  }
+ 
+  addTodo() {
+    switch (this.selectedQuadrant) {
+      case 'q1':
+        this.todo.color = 'primary';
+        break;
+      case 'q2':
+        this.todo.color = 'secondary';
+        break;
+      case 'q3':
+        this.todo.color = 'tertiary';
+        break;
+      case 'q4':
+        this.todo.color = 'warning';
+        break;
+    }
+    this[this.selectedQuadrant].push(this.todo);
+    this.todo = { value: '', color: '' };
+  }
 }
+
